3684558b8c2b1edfd3d3c2ff7a3b13d644e4e912
Daniel <daniel_ye.nz@hotmail.com>
2023-06-30 22:28:01 -0700

    add styles

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig';
 
 const Room = () => {
+
+    const [messages, setMessages] = useState([])
+    
+    useEffect(() => {
+      getMessages()
+    }, [])
+    
+
+    const getMessages = async () => {
+        const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES)
+        console.log('RESPONSE:', response)
+        setMessages(response.documents)
+    }
+
     return (
-        <div>Room</div>
+        <main className="container">
+
+            <div className="room--container">
+                <div>
+                    {messages.map(message => (
+                        <div key={message.$id} className="message--wrapper">
+
+                            <div className="message--header">
+                                <small className="message-timestamp">
+                                    {message.$createdAt}
+                                </small>
+                            </div>
+
+                            <div className="message--body">
+                                <span>{message.body}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </main>
     )
 }
 
