9ece952f2202f7a51a37590d10f37616159df1ee
Daniel <daniel_ye.nz@hotmail.com>
2023-06-30 22:37:20 -0700

    create messages

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,14 +1,34 @@
 import React, {useState, useEffect} from 'react'
 import { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig';
+import { ID } from "appwrite";
 
 const Room = () => {
 
     const [messages, setMessages] = useState([])
+    const [messageBody, setMessageBody] = useState('')
     
     useEffect(() => {
       getMessages()
     }, [])
     
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        let payload = {
+            body: messageBody
+        }
+
+        let response = await databases.createDocument(
+            DATABASE_ID,
+            COLLECTION_ID_MESSAGES,
+            ID.unique(),
+            payload
+        )
+
+        console.log(response)
+
+        setMessageBody('')
+    }
 
     const getMessages = async () => {
         const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES)
@@ -20,6 +40,25 @@ const Room = () => {
         <main className="container">
 
             <div className="room--container">
+
+            <form onSubmit={handleSubmit} id="message--form">
+                <div>
+                    <textarea
+                        required
+                        maxLength="1000"
+                        placeholder="Say something..."
+                        onChange={(e) => {setMessageBody(e.target.value)}}
+                        value={messageBody}
+                    >
+                        
+                    </textarea>
+                </div>
+
+                <div className="send-btn--wrapper">
+                    <input className="btn btn--secondary" type="submit" value="send" />
+                </div>
+            </form>
+
                 <div>
                     {messages.map(message => (
                         <div key={message.$id} className="message--wrapper">
