e5f8ee063ecef33e0c13656afbfbce811c5a3b7a
Daniel <daniel_ye.nz@hotmail.com>
2023-07-06 23:24:31 -0700

    add login feature

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,19 +1,38 @@
 import { useState, useEffect, createContext, useContext } from "react";
+import { account } from "../appwriteConfig";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({children}) => {
 
+    const navigate = useNavigate();
+
     const [loading, setLoading] = useState(true)
-    const [user, setUser] = useState(true)
+    const [user, setUser] = useState(null)
 
     useEffect(() => {
         setLoading(false)
     }, [])
     
+    const handleUserLogin = async (e, credentials) => {
+        e.preventDefault()
+
+        try {
+            const response = await account.createEmailSession(credentials.email, credentials.password);
+            console.log('logged in!', response)
+            const accountDetails = account.get();
+            setUser(accountDetails)
+
+            navigate('/')
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     const contextData = {
-        user
+        user,
+        handleUserLogin,
     }
 
     return <AuthContext.Provider value={contextData}> 
